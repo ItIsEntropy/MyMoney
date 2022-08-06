3c113305a95c2d1115c4a070ede1be5f5a895dc3
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.com>
2022-08-06 22:16:07 +0200

    most func bodies

--- geektrust.py
+++ geektrust.py
@@ -1,34 +1,73 @@
-from io import FileIO
 import sys
+from io import FileIO
 from typing import Dict, List
 from pathlib import Path
 import numpy as np
 
+months: Dict[int, str] = {
+    0: 'JANUARY',
+    1: 'FEBUARY', 
+    2: 'MARCH', 
+    3: 'APRIL', 
+    4: 'MAY', 
+    5: 'JUNE', 
+    6: 'JULY', 
+    7: 'AUGUST', 
+    8: 'SEPTEMBER', 
+    9: 'OCTOBER',
+    10: 'NOVEMBER',
+    11: 'DECEMBER'
+}
 pre_balance_portfolio: Dict[str, np.ndarray] = np.zeros(3)
 post_balance_portfolio: Dict[str, np.ndarray] = np.zeros(3)
 sip_ammount: np.array = np.zeros(3)
+current_month: int = 0;
+change_percentages: Dict[str, np.ndarray] = np.zeros(3)
 
-def perform_sip(values: np.ndarray):
+def perform_rebalance():
+    if current_month < 6:
+        print('CANNOT_REBALANCE')
+        return
     pass
 
-def perform_allocate(values: np.array):
-    pre_balance_portfolio['JANUARY'] = np.array()
-    pass
+def increment_month():
+    if current_month == 12:
+        current_month = 0
+    else:
+        current_month += 1
+    if current_month == 6 or current_month == 12:
+        perform_rebalance()
 
+def perform_sip(values: List = None):
+    if current_month < 2:
+        print('ERROR: cannot do a sip in JANUARY')
+        return
+    if values is not None:
+        sip_ammount = np.array(values)
+    pre_balance_portfolio[months[current_month]] = np.sum(sip_ammount, pre_balance_portfolio[months[current_month - 1]])
+    increment_month()
+
+def perform_allocate(values: List):
+    if current_month > 1:
+        print('Error: can only allocate in January')
+    pre_balance_portfolio['JANUARY'] = np.array(values)
+    increment_month()
+    
 def perform_change(percentages: np.ndarray, month: str):
     pass
 
 def perform_balance(month: str):
-    pass
-
-def perform_rebalance():
-    pass
+    _ = [print(ammount, end=" ") for ammount in np.floor(post_balance_portfolio[month]).tolist() ]
 
 def process_commands(command_file: str):
     line: str = command_file.readline()
+    args = line.split(' ')
+    if args[0] == 'ALLOCATE':
+        perform_allocate(args[1:])
+    elif args[0] == 'SIP':
+        perform_sip()
     pass
 
-commands: Dict[str, function] = {''}
 
 def read_file(file_path: Path):
     with open(file_path, mode='r') as f:
