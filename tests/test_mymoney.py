9e69286f63c0d9cc617bc2fdc5f337ef724a28f6
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.com>
2022-08-07 04:17:53 +0200

    unittests

--- tests/test_mymoney.py
+++ tests/test_mymoney.py
@@ -8,14 +8,20 @@ class MyMoneyBalalncerTest(unittest.TestCase):
     test_fail_dir: Path = Path(os.getcwd()).joinpath('error_dir')
     test_file_dir: Path = Path(os.getcwd()).joinpath('sample_input')
     def setUp(self) -> None:
-        os.makedir(self.test_fail_dir)
+        os.mkdir(self.test_fail_dir)
         
     def tearDown(self) -> None:
         Path.rmdir(self.test_fail_dir)
         
     def test_main_wrong_dir(self):
         result = os.system(f'python -m geektrust {self.test_fail_dir}')
+        self.assertEqual(result, -2)
+
+    def test_main_fille_does_not_exist(self):
+        test_file = self.test_file_dir.joinpath('ghtbfd')
+        result = os.system(f'python -m geektrust {test_file}')
         self.assertEqual(result, -1)
+
 
     def test_full_app(self):
         test_file = self.test_file_dir.joinpath('sample1.txt')
